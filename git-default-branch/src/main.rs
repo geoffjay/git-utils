1ee9cf88d693d01a5014ab92e54e03f38d9279a5
Geoff Johnson <geoff.jay@gmail.com>
2024-01-02 20:34:41 -0800

    chore: remove unused function

--- git-default-branch/src/main.rs
+++ git-default-branch/src/main.rs
@@ -29,26 +29,3 @@ fn run_git_command() -> Result<(), Error> {
 
     Ok(())
 }
-
-fn run_git_test() -> Result<(), Error> {
-    let repo_root = std::env::args().nth(1).unwrap_or(".".to_string());
-    let repo = Repository::open(repo_root.as_str()).expect("Couldn't open repository");
-    let mut remote = repo
-        .find_remote("origin")
-        .expect("Couldn't find remote 'origin'");
-    let authenticator = GitAuthenticator::default();
-    let git_config = git2::Config::open_default().unwrap();
-    let mut callbacks = RemoteCallbacks::new();
-
-    callbacks.credentials(authenticator.credentials(&git_config));
-
-    let connection = remote
-        .connect_auth(Direction::Fetch, Some(callbacks), None)
-        .expect("Couldn't connect to remote");
-
-    for head in connection.list()?.iter() {
-        debug!("{}\t{}", head.oid(), head.name());
-    }
-
-    Ok(())
-}
